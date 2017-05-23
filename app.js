df531925507be7905f6dde7d47755c6551f4d4ea
Peter Sabatini <peter.sabatini@logicsolutions.com>
2017-05-23 09:51:10 -0400

    Add CORS

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = require('express')();
 const routes = require('./routes');
 const PORT = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //  Connect all our routes to our application
 app.use('/', routes);
 
